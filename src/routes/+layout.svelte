aaa7c6a28754009760388fad8b9cefcd672df6d8
Nico-Mayer <nico.m94@icloud.com>
2023-07-01 08:31:25 +0200

    init

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,10 +1,23 @@
 <script lang="ts">
-	import '../app.css'
-	import ThemeToggle from '$lib/ThemeToggle.svelte'
+	import { invalidate } from '$app/navigation'
+	import { onMount } from 'svelte'
+
+	export let data
+
+	let { supabase, session } = data
+	$: ({ supabase, session } = data)
+
+	onMount(() => {
+		const {
+			data: { subscription },
+		} = supabase.auth.onAuthStateChange((event, _session) => {
+			if (_session?.expires_at !== session?.expires_at) {
+				invalidate('supabase:auth')
+			}
+		})
+
+		return () => subscription.unsubscribe()
+	})
 </script>
 
-<main
-	class="flex h-screen bg-nord-4 w-screen text-nord-0 relative dark:bg-nord-0 dark:text-nord-6">
-	<ThemeToggle />
-	<slot />
-</main>
+<slot />
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,28 +1,38 @@
 <script lang="ts">
-	let shadow = false
+	export let data
+	let { supabase } = data
+	$: ({ supabase } = data)
 
-	function toggleShadow() {
-		shadow = !shadow
+	let email: string
+	let password: string
+
+	const handleSignUp = async () => {
+		await supabase.auth.signUp({
+			email,
+			password,
+			options: {
+				emailRedirectTo: `${location.origin}/auth/callback`,
+			},
+		})
+	}
+
+	const handleSignIn = async () => {
+		await supabase.auth.signInWithPassword({
+			email,
+			password,
+		})
+	}
+
+	const handleSignOut = async () => {
+		await supabase.auth.signOut()
 	}
 </script>
 
-<section class="flex flex-col m-auto space-y-10 items-center">
-	<div
-		class="rounded flex bg-nord-6 h-52 shadow-nord-15 w-52 dark:bg-nord-3 "
-		class:shadow-xl={shadow}>
-		<h1 class="font-bold m-auto">lean-kit</h1>
-	</div>
-	<button
-		class="rounded flex bg-nord-5 h-10 p-2 w-10 gap-2 items-center dark:bg-nord-2"
-		on:click={toggleShadow}>
-		<div
-			class="i-tabler-shadow animate__fast text-2xl"
-			class:animate__wobble={shadow}
-			class:animate__swing={!shadow}
-			class:i-tabler-shadow-off={!shadow} />
-		<!-- <iconify-icon
-			icon=
-			
-			class="" /> -->
-	</button>
-</section>
+<form on:submit={handleSignUp}>
+	<input name="email" bind:value={email} />
+	<input type="password" name="password" bind:value={password} />
+	<button>Sign up</button>
+</form>
+
+<button on:click={handleSignIn}>Sign in</button>
+<button on:click={handleSignOut}>Sign out</button>
