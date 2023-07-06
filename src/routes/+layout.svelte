ba2a14fbbb629a805964bc2dd021245854597de4
Nico-Mayer <nico.m94@icloud.com>
2023-07-06 19:24:35 +0200

    moved meta tags to app.html

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -43,67 +43,6 @@
 	<meta
 		name="description"
 		content="Effortlessly store your JSON data and seamlessly access it anywhere through generated API routes." />
-
-	<!-- Google / Search Engine Tags -->
-	<meta itemprop="name" content="JSON Bucket" />
-	<meta
-		itemprop="description"
-		content="Effortlessly store your JSON data and seamlessly access it anywhere through generated API routes." />
-	<meta
-		itemprop="image"
-		content="https://github.com/Nico-Mayer/json-bucket/blob/main/static/meta.png?raw=true" />
-
-	<!-- Facebook Meta Tags -->
-	<meta property="og:url" content="https://json-bucket.vercel.app/" />
-	<meta property="og:type" content="website" />
-	<meta property="og:title" content="JSON Bucket" />
-	<meta
-		property="og:description"
-		content="Effortlessly store your JSON data and seamlessly access it anywhere through generated API routes." />
-	<meta
-		property="og:image"
-		content="https://github.com/Nico-Mayer/json-bucket/blob/main/static/meta.png?raw=true" />
-
-	<!-- Twitter Meta Tags -->
-	<meta name="twitter:card" content="summary_large_image" />
-	<meta name="twitter:title" content="JSON Bucket" />
-	<meta
-		name="twitter:description"
-		content="Effortlessly store your JSON data and seamlessly access it anywhere through generated API routes." />
-	<meta
-		name="twitter:image"
-		content="https://github.com/Nico-Mayer/json-bucket/blob/main/static/meta.png?raw=true" />
-
-	<!-- Discord Meta Tags -->
-	<meta property="og:title" content="JSON Bucket" />
-	<meta
-		property="og:description"
-		content="Effortlessly store your JSON data and seamlessly access it anywhere through generated API routes." />
-	<meta
-		property="og:image"
-		content="https://github.com/Nico-Mayer/json-bucket/blob/main/static/meta.png?raw=true" />
-	<meta property="og:url" content="https://json-bucket.vercel.app/" />
-	<meta property="theme-color" content="#7289DA" />
-
-	<!-- Reddit Meta Tags -->
-	<meta property="og:title" content="JSON Bucket" />
-	<meta
-		property="og:description"
-		content="Effortlessly store your JSON data and seamlessly access it anywhere through generated API routes." />
-	<meta
-		property="og:image"
-		content="https://github.com/Nico-Mayer/json-bucket/blob/main/static/meta.png?raw=true" />
-	<meta property="og:url" content="https://json-bucket.vercel.app/" />
-
-	<!-- Github Meta Tags -->
-	<meta property="og:url" content="https://json-bucket.vercel.app/" />
-	<meta property="og:title" content="JSON Bucket" />
-	<meta
-		property="og:description"
-		content="Effortlessly store your JSON data and seamlessly access it anywhere through generated API routes." />
-	<meta
-		property="og:image"
-		content="https://github.com/Nico-Mayer/json-bucket/blob/main/static/meta.png?raw=true" />
 </svelte:head>
 
 <main
