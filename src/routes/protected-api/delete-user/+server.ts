c3cb481c049cd33d4942368464359a1913e367fe
Nico-Mayer <nico.m94@icloud.com>
2023-07-07 10:43:59 +0200

    guard for test user

--- src/routes/protected-api/delete-user/+server.ts
+++ src/routes/protected-api/delete-user/+server.ts
@@ -1,7 +1,12 @@
+import { PUBLIC_DEMO_EMAIL } from '$env/static/public'
+
 export const DELETE = async ({ locals }) => {
 	const session = await locals.getSession()
 	const supabase = locals.supabase
 	if (!session) return new Response('Unauthorized', { status: 401 })
+	if (session.user.email !== PUBLIC_DEMO_EMAIL) {
+		return new Response('Test user can not be deleted', { status: 401 })
+	}
 
 	await supabase.rpc('delete_user')
 	await supabase.auth.signOut()
